c8e26ec0d47c96e92bd5f38c25635e4f71fa29dc
Inqlab Muradov <inqlab.muradov.777@gmail.com>
2024-07-17 15:55:49 +0400

    second commit

--- app/src/main/java/com/inqlab/ortalamaapp/MainActivity2.kt
+++ app/src/main/java/com/inqlab/ortalamaapp/MainActivity2.kt
@@ -2,20 +2,25 @@ package com.inqlab.ortalamaapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.inqlab.ortalamaapp.databinding.ActivityMain2Binding
+import java.io.Console
+import kotlin.properties.Delegates
 
 class MainActivity2 : AppCompatActivity() {
-
 
     private lateinit var binding:ActivityMain2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        var  listSize = 0
 
         binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -27,53 +32,102 @@ class MainActivity2 : AppCompatActivity() {
         binding.radioGroup.setOnCheckedChangeListener { radioGroup, i ->
             when(binding.radioGroup.checkedRadioButtonId) {
                 R.id.radioButton3->{
+                    listSize = 3
                     binding.point4.invisible()
                     binding.credit4.invisible()
                     binding.point5.invisible()
                     binding.credit5.invisible()
                 }
                 R.id.radioButton4->{
+                    listSize = 4
+                    binding.point4.visible()
+                    binding.credit4.visible()
                     binding.point5.invisible()
                     binding.credit5.invisible()
+                }
+                R.id.radioButton5->{
+                    binding.point4.visible()
+                    binding.credit4.visible()
+                    binding.point5.visible()
+                    binding.credit5.visible()
+                    listSize = 5
                 }
             }
         }
 
-
-
         binding.buttonCalculate.setOnClickListener {
 
-            val point1 = binding.point1.text.toString().toInt()
-            val point2 = binding.point2.text.toString().toInt()
-            val point3 = binding.point3.text.toString().toInt()
-            val point4 = binding.point4.text.toString().toInt()
-            val point5 = binding.point5.text.toString().toInt()
-
-            val credit1 =binding.credit1.text.toString().toInt()
-            val credit2 =binding.credit2.text.toString().toInt()
-            val credit3 =binding.credit3.text.toString().toInt()
-            val credit4 =binding.credit4.text.toString().toInt()
-            val credit5 =binding.credit5.text.toString().toInt()
-
             val creditList = ArrayList<Int>()
-            creditList.add(credit1)
-            creditList.add(credit2)
-            creditList.add(credit3)
-            creditList.add(credit4)
-            creditList.add(credit5)
+            val creditAndPoint = ArrayList<Int>()
+
+            if(listSize==3){
+                val point1 = binding.point1.text.toString().toInt()
+                val point2 = binding.point2.text.toString().toInt()
+                val point3 = binding.point3.text.toString().toInt()
+
+                val credit1 =binding.credit1.text.toString().toInt()
+                val credit2 =binding.credit2.text.toString().toInt()
+                val credit3 =binding.credit3.text.toString().toInt()
+
+                creditList.add(credit1)
+                creditList.add(credit2)
+                creditList.add(credit3)
+
+                creditAndPoint.add(point1*credit1)
+                creditAndPoint.add(point2*credit2)
+                creditAndPoint.add(point3*credit3)
+
+            }else if (listSize==4){
+                val point1 = binding.point1.text.toString().toInt()
+                val point2 = binding.point2.text.toString().toInt()
+                val point3 = binding.point3.text.toString().toInt()
+                val point4 = binding.point4.text.toString().toInt()
+
+                val credit1 =binding.credit1.text.toString().toInt()
+                val credit2 =binding.credit2.text.toString().toInt()
+                val credit3 =binding.credit3.text.toString().toInt()
+                val credit4 =binding.credit4.text.toString().toInt()
+
+                creditList.add(credit1)
+                creditList.add(credit2)
+                creditList.add(credit3)
+                creditList.add(credit4)
+
+                creditAndPoint.add(point1*credit1)
+                creditAndPoint.add(point2*credit2)
+                creditAndPoint.add(point3*credit3)
+                creditAndPoint.add(point4*credit4)
+            }else{
+                val point1 = binding.point1.text.toString().toInt()
+                val point2 = binding.point2.text.toString().toInt()
+                val point3 = binding.point3.text.toString().toInt()
+                val point4 = binding.point4.text.toString().toInt()
+                val point5 = binding.point5.text.toString().toInt()
+
+                val credit1 =binding.credit1.text.toString().toInt()
+                val credit2 =binding.credit2.text.toString().toInt()
+                val credit3 =binding.credit3.text.toString().toInt()
+                val credit4 =binding.credit4.text.toString().toInt()
+                val credit5 =binding.credit5.text.toString().toInt()
+
+                creditList.add(credit1)
+                creditList.add(credit2)
+                creditList.add(credit3)
+                creditList.add(credit4)
+                creditList.add(credit5)
+
+                creditAndPoint.add(point1*credit1)
+                creditAndPoint.add(point2*credit2)
+                creditAndPoint.add(point3*credit3)
+                creditAndPoint.add(point4*credit4)
+                creditAndPoint.add(point5*credit5)
+
+            }
 
             var creditSum = 0
-
             for (i in creditList) {
                  creditSum = creditSum + i
             }
-
-            val creditAndPoint = ArrayList<Int>()
-            creditAndPoint.add(point1*credit1)
-            creditAndPoint.add(point2*credit2)
-            creditAndPoint.add(point3*credit3)
-            creditAndPoint.add(point4*credit4)
-            creditAndPoint.add(point5*credit5)
 
             var creditAndPointSum = 0
 
@@ -82,6 +136,7 @@ class MainActivity2 : AppCompatActivity() {
             }
 
             val ortalama = creditAndPointSum / creditSum
+
 
             if (ortalama>=50) {
                 val intent = Intent(this@MainActivity2,MainActivity3::class.java)
@@ -96,6 +151,7 @@ class MainActivity2 : AppCompatActivity() {
                 startActivity(intent)
                 finish()
             }
+
         }
     }
 }
